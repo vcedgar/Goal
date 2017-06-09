f7a7eb4985613fdd7f87a5cd8f565fcde0269530
vcedgar <vatriciaedgar@gmail.com>
2017-06-08 17:08:33 -0700

    Main window made, controller added, goal started

---
+++ Goal/src/application/Controler.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controler {
+
+    @FXML protected void addGoal(ActionEvent event){
+    	System.out.println("Yo");
+    }
+    
+    
+}
--- Goal/src/application/Main.java
+++ Goal/src/application/Main.java
@@ -1,24 +1,26 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Window.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
 	
 	public static void main(String[] args) {
 		launch(args);
